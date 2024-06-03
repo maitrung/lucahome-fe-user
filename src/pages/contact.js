9c931fbda5d3a604843389f1b1e401356dcf772a
lucahome <taimd@payme.vn>
2024-06-03 09:48:49 +0700

    (feat)[CONTACT] remove google map

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -51,7 +51,7 @@ function Contact() {
                                     <tbody>
                                         <tr>
                                             <td className="c-o">Địa chỉ:</td>
-                                            <td>Biên Hoà</td>
+                                            <td>Cần Thơ</td>
                                         </tr>
                                         <tr>
                                             <td className="c-o">Điện thoại :</td>
@@ -66,14 +66,14 @@ function Contact() {
                             </div>
                         </div>
                     </div>
-                    <div className="map">
+                    {/* <div className="map">
                         <iframe
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.0606825994123!2d-72.8735845851828!3d40.760690042573295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e85b24c9274c91%3A0xf310d41b791bcb71!2sWilliam%20Floyd%20Pkwy%2C%20Mastic%20Beach%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1578582744646!5m2!1sen!2sbd"
                             height="470"
                             // style={border:0;
                             allowfullscreen
                         ></iframe>
-                    </div>
+                    </div> */}
                 </div>
             </section>
 
