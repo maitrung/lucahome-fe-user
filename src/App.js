fbae955b2b1c9a836aff6e715c003ac4f2b2ad51
TuanVo <luca.home.development@gmail.com>
2024-08-10 01:52:28 +0700

    V2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import HomeStay from './pages/homeStay';
+import HomeStayV2 from './pages/homeStayV2';
 import Index from './pages/index';
 import HomeStayDetail from './pages/homeStayDetail';
+import HomeStayDetailV2 from './pages/homeStayDetailV2';
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom'
 import Contact from './pages/contact';
 import AdminDashBoard from './pages/adminDashboard';
@@ -13,7 +15,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Index />} />
           <Route path='/homestay' element={<HomeStay />} />
+          <Route path='/homestayV2' element={<HomeStayV2 />} />
           <Route path='/detail' Component={HomeStayDetail} />
+          <Route path='/detailV2' Component={HomeStayDetailV2} />
           <Route path='/contact' Component={Contact} />
           {/* <Route path='/dashboard' Component={AdminDashBoard} /> */}
 
---
+++ src/pages/homeStayV2.js
@@ -0,0 +1,250 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import Menu from './menu';
+import axios from 'axios';
+import numeral from 'numeral';
+import { Link } from 'react-router-dom';
+import Footer from './footer';
+
+function HomeStayV2() {
+    const [homestay, setHomeStay] = useState([]);
+
+    useEffect(() => {
+        const fetchHomeStay = async () => {
+            try {
+                let homestayResult = await axios.post(`${process.env.REACT_APP_URL_BACKEND || 'https://luca-home.vercel.app'}/room/search`);
+                homestayResult = homestayResult?.data;
+                if (homestayResult?.code === 1000) {
+                    setHomeStay(homestayResult?.data?.rooms);
+                }
+            } catch (error) {
+                console.log(`ERROR when call get list homestay ${error.message} -- ${JSON.stringify(error)}`);
+            }
+        }
+        fetchHomeStay();
+    }, []);
+
+    return (
+        <Fragment>
+            <Menu />
+
+            <div className="breadcrumb-section">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="breadcrumb-text">
+                                <h2 style={{ fontFamily: 'Gilroy-Regular', color: '#fcead6' }}>Danh sách Home Stay</h2>
+                                <div className="bt-option">
+                                    <a href="#" style={{ fontFamily: 'Gilroy-Regular', color: '#fcead6' }}>Trang chủ</a>
+                                    <span style={{ fontFamily: 'Gilroy-Regular', color: '#fcead6' }}>Home stay</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <section className="rooms-section spad">
+                <div className="container">
+                    <div className="row">
+                        {homestay.map((item, index) => {
+                            return <div key={index} className="col-lg-4 col-md-6">
+                                <div className="room-item">
+                                    <img src={item?.images?.[0]} alt="" />
+                                    <div className="ri-text">
+                                        <h4 style={{ color: '#fcead6' }}>{item?.name}</h4>
+                                        <h3 style={{ color: '#fcead6' }}>{numeral(item?.priceList?.overNight || 0).format('0,0')} vnđ<span style={{ color: '#fcead6' }}>/Qua đêm</span></h3>
+                                        <table>
+                                            <tbody>
+                                                {/* <tr>
+                                                    <td className="r-o">Diện tích::</td>
+                                                    <td>42 m2</td>
+                                                </tr> */}
+                                                {/* <tr>
+                                                    <td className="r-o">Giới hạn người::</td>
+                                                    <td>Không giới hạn</td>
+                                                </tr>
+                                                <tr>
+                                                    <td className="r-o">Phòng:</td>
+                                                    <td>2 phòng</td>
+                                                </tr>
+                                                <tr>
+                                                    <td className="r-o">Dịch vụ:</td>
+                                                    <td>{item?.services}</td>
+                                                </tr> */}
+                                            </tbody>
+                                        </table>
+                                        <Link to={'/detailV2'} state={{ data: { homeStay: item, from: new Date() } }} className="primary-btn" style={{ color: '#fcead6' }}>
+                                            Chi tiết
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        })}
+
+                        {/* <div className="col-lg-4 col-md-6">
+                            <div className="room-item">
+                                <img src="assets/img/room/room-2.jpg" alt="" />
+                                <div className="ri-text">
+                                    <h4>Secret Garden</h4>
+                                    <h3>350.000 vnđ<span>/Qua đêm</span></h3>
+                                    <table>
+                                        <tbody>
+                                            <tr>
+                                                <td className="r-o">Diện tích::</td>
+                                                <td>42 m2</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="r-o">Giới hạn người::</td>
+                                                <td>Không giới hạn</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="r-o">Phòng:</td>
+                                                <td>2 phòng</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="r-o">Dịch vụ:</td>
+                                                <td>Wifi, Màn hình rộng, Bồn tắm, Hồ bơi...</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <a href="./room-details.html" className="primary-btn">Chi tiết</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6">
+                            <div className="room-item">
+                                <img src="assets/img/room/room-3.jpg" alt="" />
+                                <div className="ri-text">
+                                    <h4>Secret Garden</h4>
+                                    <h3>350.000 vnđ<span>/Qua đêm</span></h3>
+                                    <table>
+                                        <tbody>
+                                            <tr>
+                                                <td className="r-o">Diện tích::</td>
+                                                <td>42 m2</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="r-o">Giới hạn người::</td>
+                                                <td>Không giới hạn</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="r-o">Phòng:</td>
+                                                <td>2 phòng</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="r-o">Dịch vụ:</td>
+                                                <td>Wifi, Màn hình rộng, Bồn tắm, Hồ bơi...</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <a href="./room-details.html" className="primary-btn">Chi tiết</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6">
+                            <div className="room-item">
+                                <img src="assets/img/room/room-4.jpg" alt="" />
+                                <div className="ri-text">
+                                    <h4>Secret Garden</h4>
+                                    <h3>350.000 vnđ<span>/Qua đêm</span></h3>
+                                    <table>
+                                        <tbody>
+                                            <tr>
+                                                <td className="r-o">Diện tích::</td>
+                                                <td>42 m2</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="r-o">Giới hạn người::</td>
+                                                <td>Không giới hạn</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="r-o">Phòng:</td>
+                                                <td>2 phòng</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="r-o">Dịch vụ:</td>
+                                                <td>Wifi, Màn hình rộng, Bồn tắm, Hồ bơi...</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <a href="./room-details.html" className="primary-btn">Chi tiết</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6">
+                            <div className="room-item">
+                                <img src="assets/img/room/room-5.jpg" alt="" />
+                                <div className="ri-text">
+                                    <h4>Secret Garden</h4>
+                                    <h3>350.000 vnđ<span>/Qua đêm</span></h3>
+                                    <table>
+                                        <tbody>
+                                            <tr>
+                                                <td className="r-o">Diện tích::</td>
+                                                <td>42 m2</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="r-o">Giới hạn người::</td>
+                                                <td>Không giới hạn</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="r-o">Phòng:</td>
+                                                <td>2 phòng</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="r-o">Dịch vụ:</td>
+                                                <td>Wifi, Màn hình rộng, Bồn tắm, Hồ bơi...</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <a href="./room-details.html" className="primary-btn">Chi tiết</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6">
+                            <div className="room-item">
+                                <img src="assets/img/room/room-6.jpg" alt="" />
+                                <div className="ri-text">
+                                    <h4>Secret Garden</h4>
+                                    <h3>350.000 vnđ<span>/Qua đêm</span></h3>
+                                    <table>
+                                        <tbody>
+                                            <tr>
+                                                <td className="r-o">Diện tích::</td>
+                                                <td>42 m2</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="r-o">Giới hạn người::</td>
+                                                <td>Không giới hạn</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="r-o">Phòng:</td>
+                                                <td>2 phòng</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="r-o">Dịch vụ:</td>
+                                                <td>Wifi, Màn hình rộng, Bồn tắm, Hồ bơi...</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <a href="./room-details.html" className="primary-btn">Chi tiết</a>
+                                </div>
+                            </div>
+                        </div> */}
+                        <div className="col-lg-12">
+                            <div className="room-pagination">
+                                <a href="#" style={{ color: '#fcead6' }}>1</a>
+                                <a href="#" style={{ color: '#fcead6' }}>2</a>
+                                <a href="#" style={{ color: '#fcead6' }}>Next <i className="fa fa-long-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <Footer />
+        </Fragment >
+    )
+}
+
+export default HomeStayV2;
